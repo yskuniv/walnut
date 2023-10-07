692da5400aa5da367cbf12dd8a3e75c7440f6b5c
ysk <ysk.univ.1007@gmail.com>
2023-10-07 09:01:03 +0900

    first commit

---
+++ lib/walnut.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "walnut/version"
+
+module Walnut
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/walnut_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Walnut do
+  it "has a version number" do
+    expect(Walnut::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
